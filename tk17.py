450bc917166a704bcc1483c4404b09c8d2b717d2
MT Ekleel <63772286+MTE1991@users.noreply.github.com>
2022-09-01 21:42:11 +0600

    Add files via upload

---
+++ tk17.py
@@ -0,0 +1,41 @@
+# same as earlier program, w/ an exit btn
+from operator import truediv
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
+
+# create the root window
+root = tk.Tk()
+root.geometry('400x300')
+root.resizable(True, True)
+root.title('Image Label and Widgets')
+
+# display an image label
+photo = ImageTk.PhotoImage(Image.open("me.PNG"))
+image_label = ttk.Label(
+    root,
+    image=photo,
+    text="HELLO, WORLD FROM MT EKLEEL",
+    compound='top'
+)
+image_label.pack()
+
+# button click event
+def btn_click():
+    new_label = ttk.Label(root, text="Thanks for clicking me!")
+    new_label.pack()
+
+# create a button
+btn = ttk.Button(
+    root,
+    text="Click Here!",
+    command=btn_click
+)
+
+btn.pack(
+    ipadx=10,
+    ipady=5,
+    expand=True
+)
+
+root.mainloop()
